36c2605d49b5bcb3905019c88568291c55c338ad
juanalbertomontoya <67620654+juanalbertomontoya@users.noreply.github.com>
2020-06-29 23:13:23 -0500

    primera subida
    acabo de subir los archivos

---
+++ page1/javascript.js
@@ -0,0 +1,12 @@
+//let animado = document.querySelectorAll(".bloque");
+//
+//function mostrarScroll() {
+//    let scrollTop = document.documentElement.scrollTop;
+//    for (var i=0; i < animado.length; i++ ) {
+//        let alturaAnimado = animado[i].offsetTop;
+//        if(alturaAnimado - 100 < scrollTop) {
+//            animado[i].style.opacity = 1;
+//            animado[i].classList.add("mostrarArriba");
+//        }
+//    }
+//}
